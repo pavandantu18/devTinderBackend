54f4f4a85e9a09bd16f8483fbd7aacee25d0f72b
pavandantu18 <pavandantu18@gmail.com>
2025-09-29 17:15:06 -0400

    query params

--- src/app.js
+++ src/app.js
@@ -15,8 +15,19 @@ const app = express();
 // })
 
 // If we want to match only specific HTTP methods, we can use app.get, app.post, etc.
+// app.get("/user",(req,res) => {
+//     res.send('Hello World from /user');
+// })
+
+app.get("/user/:id/:name",(req,res) => {
+    console.log(req.params);
+    res.send(`Hello World from /user with id ${req.params.id}`);
+})
+
 app.get("/user",(req,res) => {
-    res.send('Hello World from /user');
+    // /user?id=123&name=pavan
+    console.log(req.query);
+    res.send(`Hello World from /user with id ${req.query.id}`);
 })
 
 app.post("/user",(req,res) => {
@@ -26,6 +37,28 @@ app.post("/user",(req,res) => {
 app.delete("/user",(req,res) => {
     res.send('user deleted');
 })
+
+// ab?c -> ac, abc, abbc
+// app.get("/ab?c",(req,res) => {
+//     res.send('Hello World from /ab?c');
+// })
+
+// ab+c -> abc, abbc, abbbc
+// app.get("/ab+c",(req,res) => {
+//     res.send('Hello World from /ab+c');
+// })
+
+// ab*c ->  abc, abbc, abbbc, abxc
+// app.get("/ab*c",(req,res) => {
+//     res.send('Hello World from /ab*c');
+// })
+
+// ab[cd]e -> abce, abde
+// app.get("/ab[cd]e",(req,res) => {
+//     res.send('Hello World from /ab[cd]e');
+// })
+
+
 
 app.listen(3000,() => {
     console.log('Server is running on port 3000');
