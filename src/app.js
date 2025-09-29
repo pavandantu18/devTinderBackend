d6fa7f787dc00be2a2f60697c92ffc0964f70644
pavandantu18 <pavandantu18@gmail.com>
2025-09-29 16:59:13 -0400

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,30 @@ const express = require('express');
 
 const app = express();
 
-app.use("/",(req,res) => {
-    res.send('Welcome to the Home Page');
+// Order of the routes matters
+
+// This will match all the HTTP methods (GET, POST, PUT, DELETE, etc.)
+
+// app.use("/test",(req,res) => {
+//     res.send('Hello World');
+// })
+
+// app.use("/",(req,res) => {
+//     res.send('Welcome to the Home Page');
+// })
+
+// If we want to match only specific HTTP methods, we can use app.get, app.post, etc.
+app.get("/user",(req,res) => {
+    res.send('Hello World from /user');
 })
 
-app.use("/test",(req,res) => {
-    res.send('Hello World');
+app.post("/user",(req,res) => {
+    res.send('user data updated');
 })
 
-
+app.delete("/user",(req,res) => {
+    res.send('user deleted');
+})
 
 app.listen(3000,() => {
     console.log('Server is running on port 3000');
